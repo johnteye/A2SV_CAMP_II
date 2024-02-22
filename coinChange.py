f0ccb36e89df0837672be33c623add8dd30b42b0
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-02-22 16:56:12 +0000

    Update coinChange.py

--- coinChange.py
+++ coinChange.py
@@ -1,3 +1,4 @@
+# top to down approach
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
@@ -22,3 +23,18 @@ class Solution:
         if res >= float("inf"):
             return -1
         return dp(amount)
+
+# bottom up approach
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount + 1) 
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+    
+        return dp[amount] if dp[amount] != float("inf") else -1
+
+      

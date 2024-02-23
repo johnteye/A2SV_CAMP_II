2220b52b9e47214405f90b8b9aeed3015d3f2443
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-02-23 09:33:33 +0000

    Update minCostClimbingStairs.py

--- minCostClimbingStairs.py
+++ minCostClimbingStairs.py
@@ -12,3 +12,17 @@ class Solution:
 
         memo = {}
         return min(dp(0), dp(1))
+
+# bottom up
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0,0]
+     
+        
+        for i in range(2, n +  1):
+            temp = dp[1]
+            dp[1] = min(dp[1] + cost[i-1], dp[0] + cost[i-2])
+            dp[0]= temp
+        
+        return dp[1]

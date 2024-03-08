ddde89b6e8c1fbe79edc30dac693c3c792950964
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-03-08 15:37:51 +0000

    Update hasValidPath.py

--- hasValidPath.py
+++ hasValidPath.py
@@ -1,28 +1,29 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        uf = {}
+        unionfind = {}
         def find(x):
-            uf.setdefault(x, x)
-            if uf[x] != x:
-                uf[x] = find(uf[x])
-            return uf[x]
-        
+            unionfind.setdefault(x,x)
+            if x == unionfind[x]:
+                return x
+            unionfind[x] = find(unionfind[x])
+            return unionfind[x]
+
         def union(x, y):
-            uf[find(y)] = find(x)
-        
-        R, C = len(grid), len(grid[0])
-        right_out = {1, 4, 6}
+            unionfind[find(y)] = find(x)
+
         left_in = {1, 3, 5}
+        right_out = {1, 4, 6}
         down_out = {2, 3, 4}
-        up_in = {2, 5, 6}
-        
-        for i in range(R):
-            for j in range(C):
-                # Connect left and right
-                if j > 0 and grid[i][j-1] in right_out and grid[i][j] in left_in:
-                    union((i, j-1), (i, j))
-                # Connect up and down
-                if i > 0 and grid[i-1][j] in down_out and grid[i][j] in up_in:
-                    union((i-1, j), (i, j))
-        
-        return find((0, 0)) == find((R-1, C-1))
+        up_in = {2,5,6}
+
+        row, col = len(grid), len(grid[0])
+        for i in range(row):
+            for j in range(col):
+                
+                if j > 0 and grid[i][j - 1] in right_out and grid[i][j] in left_in:
+                    union((i, j - 1), (i, j))
+
+                if i > 0 and grid[i - 1][j] in down_out and grid[i][j] in up_in:
+                    union((i -1 , j), (i, j))
+
+        return find((0, 0)) == find((row-1, col-1))

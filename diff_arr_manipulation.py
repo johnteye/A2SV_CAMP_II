a6bac2f9d808daf961db6a8b0c83a26df3fecaa5
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-03-27 10:46:57 +0000

    Create diff_arr_manipulation.py

---
+++ diff_arr_manipulation.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    diff = [0]* (n+ 2)
+    for l, r, val in queries:
+        diff[l] += val
+        diff[r+1] -= val
+        
+    prefix = [0]
+    for val in diff:
+        prefix.append(prefix[-1] + val)
+    return max(prefix)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

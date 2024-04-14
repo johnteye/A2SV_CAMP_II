456d649703f098effdb70072bd0fa784593e1613
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-04-14 21:09:48 +0000

    Create trappingrainwater.py

---
+++ trappingrainwater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        stack = []
+        n = len(height)
+        res = 0
+
+        for i in range(n):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                distance = i - stack[-1] - 1
+                bounded_height = min(height[i], height[stack[-1]]) - height[top]
+                res += distance * bounded_height
+            stack.append(i)
+
+        return res
